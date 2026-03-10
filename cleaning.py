92aa449ff267c7614cf1662c1aa8fd734cd752c7
YunfeiYu0318 <yunfeiyu2001@outlook.com>
2026-03-09 21:22:21 -0400

    Add clean_labs function and update main

--- cleaning.py
+++ cleaning.py
@@ -21,13 +21,19 @@ from typing import List, Tuple, Callable
 # === Global Constants === 
 # Path to data folder
 RAW_DATA_PATH = "raw/"
+OUTPUT_DATA_PATH = "data/"
 
 # Relevant variables
 POSTOP_COLS = ["LOG_ID", "MRN", "SMRTDTA_ELEM_VALUE"]
 INFO_COLS = ["LOG_ID", "MRN", "BIRTH_DATE", "HEIGHT", "WEIGHT", "SEX", "AN_START_DATETIME"]
 LABS_COLS = ["LOG_ID", "MRN", "Lab Code", "Lab Name", "Observation Value", "Measurement Units", "Collection Datetime"]
 
-# === Helper Functions for Data Preprocessing ===
+# Lab tests
+LAB_TESTS = ['Leukocytes', 'pH', 'Hematocrit', 'C reactive protein', 'Lactate']
+
+# === Helper Functions ===
+
+# For preprocessing
 
 def remove_invalid_log_ids(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -56,6 +62,29 @@ def remove_invalid_log_ids(df: pd.DataFrame) -> pd.DataFrame:
     
     return df_cleaned
 
+def pre_process(df: pd.DataFrame, cols_to_keep: List[str]) -> pd.DataFrame:
+    """
+    Pre-process df by 
+    - Selecting and retaining only cols_to_keep
+    - Removing invalid LOG_ID values (those correspond to multiple MRNs)
+
+    Return cleaned df
+    """
+    # Raise an error if any of cols_to_keep is missing from df
+    missing_cols = [col for col in cols_to_keep if col not in df.columns]
+    if missing_cols:
+        raise KeyError(f"The following required columns are missing: {missing_cols}")
+
+    # Select relevant columns
+    # use .copy() to make sure df is a dataframe not a slice
+    df = df[cols_to_keep].copy()
+
+    # Drop invalid LOG_IDs
+    df_cleaned = remove_invalid_log_ids(df)
+    
+    return df_cleaned
+
+# For missing value imputation
 
 def partition_by_completeness(df: pd.DataFrame,
                               target: str,
@@ -130,8 +159,49 @@ def run_lmm_imputation(df: pd.DataFrame,
 
     return df_filled
 
-    
+# For filtering lab tests
 
+def find_lab_name_code_pair(df: pd.DataFrame,
+                            most_common_tests: List[str],
+                            pre_defined_tests: List[str] = LAB_TESTS) -> pd.DataFrame:
+    """
+    Find the most common lab name-code pair for each test in most_common_tests and pre_defined_tests
+
+    LAB_TESTS = ['Leukocytes', 'pH', 'Hematocrit', 'C reactive protein', 'Lactate']
+    """
+    target_tests = pre_defined_tests + most_common_tests
+    selected_pairs = []
+
+    for test in target_tests:
+        # Case-sensitive filtering for pH
+        if test == "pH":
+            test_matches = df[df["Lab Name"].str.contains(r'\bpH\b', case = True, na = False)]
+        
+        # Filtering for Lactate (exclude D-lactate)
+        elif test == "Lactate":
+            test_matches = df[
+                df["Lab Name"].str.contains("Lactate", case = False, na = False) & 
+                ~df["Lab Name"].str.contains("D-lactate", case = False, na = False)
+            ]
+        
+        # Case-insensitive filtering for all other tests
+        else:
+            test_matches = df[df["Lab Name"].str.contains(test, case = False, na = False)]
+    
+        if not test_matches.empty:
+            # Choose the most common Lab Name value
+            most_common_name = test_matches["Lab Name"].value_counts().idxmax()
+            
+            # Find the most common Lab Code associated with that specific Lab Name
+            name_subset = test_matches[test_matches["Lab Name"] == most_common_name]
+            most_common_code = name_subset["Lab Code"].value_counts().idxmax()
+            
+            selected_pairs.append({"Lab Name": most_common_name, "Lab Code": most_common_code})
+
+    # convert to a df
+    tests_df = pd.DataFrame(selected_pairs)
+    
+    return tests_df
 
 # === Functions for Data Cleaning ===
 
@@ -147,19 +217,10 @@ def clean_complications(postop: pd.DataFrame, cols_to_keep: List[str] = POSTOP_C
 
     print("Starting cleaning patient postoperative complications data ...")
 
-    # Raise an error if any of cols_to_keep is missing from postop df
-    missing_cols = [col for col in cols_to_keep if col not in postop.columns]
-    if missing_cols:
-        raise KeyError(f"The following required columns are missing: {missing_cols}")
+    postop_cleaned = pre_process(postop, cols_to_keep)
 
-    # Select relevant columns
-    # use .copy() to make sure postop is a df not a slice
-    postop = postop[cols_to_keep].copy()
-
-    # Drop invalid LOG_IDs
-    postop_cleaned = remove_invalid_log_ids(postop)
-
-    print("Cleaning complete.")
+    print(f"Cleaning complete. Final Row Count: {len(postop_cleaned)}")
+    # should be 203939
 
     return postop_cleaned
     
@@ -185,16 +246,7 @@ def clean_information(info: pd.DataFrame,
 
     # --- PRE-PROCESSING ---
 
-    # Raise an error if any of cols_to_keep is missing from info df
-    missing_cols = [col for col in cols_to_keep if col not in info.columns]
-    if missing_cols:
-        raise KeyError(f"The following required columns are missing: {missing_cols}")
-
-    # Select relevant columns
-    info = info[cols_to_keep].copy()
-    
-    # Drop invalid LOG_IDs
-    info_cleaned = remove_invalid_log_ids(info)
+    info_cleaned = pre_process(info, cols_to_keep)
 
     # --- CONVERT VARIABLES ---
     
@@ -218,19 +270,6 @@ def clean_information(info: pd.DataFrame,
     # --- HANDLE MISSING VALUES ---
 
     print("Handling missing values in patient_information data...")
-
-    # Check and report number of missing values
-    na_count_sex = info_cleaned[info_cleaned["SEX"] == "Unknown"].shape[0]
-    print(f"Missing value count in SEX: {na_count_sex}")
-
-    na_count_time = info_cleaned[info_cleaned["AN_START_DATETIME"].isna()].shape[0]
-    print(f"Missing value count in AN_START_DATETIME: {na_count_time}")
-
-    na_count_height = info_cleaned[info_cleaned["HEIGHT"].isna()].shape[0]
-    print(f"Missing value count in HEIGHT: {na_count_height}")
-
-    na_count_weight = info_cleaned[info_cleaned["WEIGHT"].isna()].shape[0]
-    print(f"Missing value count in WEIGHT: {na_count_weight}")
 
     # Remove "Unknown" value in SEX variable
     info_cleaned = info_cleaned[info_cleaned["SEX"] != "Unknown"].reset_index(drop = True)
@@ -257,18 +296,73 @@ def clean_information(info: pd.DataFrame,
                                     group_col = "MRN")
 
     # Step 3: drop remaining NAs (rows still missing both HEIGHT and WEIGHT)
+    print("Drop remaining NAs in HEIGHT and WEIGHT")
     info_filled = info_filled.dropna(subset = ["HEIGHT", "WEIGHT"], how = "all").reset_index(drop = True)
 
     print(f"Cleaning complete. Final Row Count: {len(info_filled)}")
+    # Note: should be 57026
 
     return info_filled
     
 
-    
-    
-    
+def clean_labs(labs: pd.DataFrame, 
+               cols_to_keep: List[str] = LABS_COLS,
+               date_format: str = "%Y-%m-%d %H:%M:%S") -> pd.DataFrame:
+    """
+    Cleans info (patient_information dataframe) by:
+    - Selecting and retaining only cols_to_keep
+    - Removing invalid LOG_ID values (those correspond to multiple MRNs)
+    - Converting variables to desired type
+    - Treating missing values
+    - Filtering for desired lab tests
 
-# def clean_labs(df: pd.DataFrame, cols_to_retain: List[str] = LABS_COLS) -> pd.DataFrame:
+    LABS_COLS = ["LOG_ID", "MRN", "Lab Code", "Lab Name", "Observation Value", "Measurement Units", "Collection Datetime"]
+    LAB_TESTS = ['Leukocytes', 'pH', 'Hematocrit', 'C reactive protein', 'Lactate']
+
+    date_format: format of Collection Datetime
+
+    Returns a cleaned lab test data containing cols_to_keep and all encounters with valid LOG_IDs.
+    """
+
+    print("Starting cleaning patient labs data ...")
+    
+    # --- PRE-PROCESSING ---
+
+    labs_cleaned = pre_process(labs, cols_to_keep)
+
+    # --- CONVERT VARIABLES ---
+    # Convert Collection Datetime to DateTime
+    labs_cleaned["Collection Datetime"] = pd.to_datetime(labs_cleaned["Collection Datetime"],
+                                                         format = date_format,
+                                                         errors = "coerce")
+
+    # --- HANDLE MISSING VALUES ---
+    # Drop rows with Observation Value = 9999999.0
+    labs_cleaned = labs_cleaned[labs_cleaned["Observation Value"] != 9999999.0]
+
+    # --- FILTER LAB TESTS ---
+    # Find the 5 most common lab tests excluding those in LAB_TESTS
+    mask = labs_cleaned["Lab Name"].str.contains('|'.join(LAB_TESTS), case = False, na = False)
+    other_tests_df = labs_cleaned[~mask]
+    top5_tests = other_tests_df["Lab Name"].value_counts().head(5).index.tolist()
+    print(f"Most common tests {top5_tests}")
+
+    # Choose most common lab name-code pair for each test
+    tests_df = find_lab_name_code_pair(df = labs_cleaned, most_common_tests = top5_tests)
+    print(f"Selected pairs: {tests_df}")
+
+    # Filter to include only rows matching the selected name-code pair
+    keys = ["Lab Name", "Lab Code"]
+    i1 = labs_cleaned.set_index(keys).index
+    i2 = tests_df.set_index(keys).index
+    
+    # filter to include only the 10 selected name-code pairs
+    labs_filtered = labs_cleaned[i1.isin(i2)].reset_index(drop=True)
+
+    print(f"Cleaning complete. Final Row Count: {len(labs_filtered)}")
+    # should be 5052813
+
+    return labs_filtered
 
 
 
@@ -295,11 +389,13 @@ def main():
             key = file.replace(".csv", "")
             raw_files[key] = df
             print(f"Successfully loaded {file}")
+            
     # Raise an error if file not found
     except FileNotFoundError as e:
         print(f"\n[Error] A required file is missing: ")
         print(f"{e}")
         return
+        
     # Raise an error if an unexpected error occurred
     except Exception as e:
         print(f"\n[Error] An unexpected error occurred while reading files: ")
@@ -310,14 +406,38 @@ def main():
     postop_raw = raw_files["patient_post_op_complications"]
     info_raw = raw_files["patient_information"]
     labs_raw = raw_files["patient_labs"]
-
     
     print("\nAll data ready for processing")
 
-
+    # --- Clean data ---
     postop = clean_complications(postop_raw)
     info = clean_information(info_raw)
-    
+    labs = clean_labs(labs_raw)
+
+    # --- Merge data ---
+    print(" --- Merging dataframes --- ")
+    merged_df = postop.merge(info, on = ["LOG_ID", "MRN"], how = "inner")
+    merged_df = merged_df.merge(labs, on = ["LOG_ID", "MRN"], how = "inner")
+    nlog = merged_df["LOG_ID"].nunique()
+    print(f"Merging complete. \nRow Count: {len(merged_df)}. \nUnique LOG ID: {nlog}")
+
+    # -- Final filtering of lab tests ---
+    print(" --- Filtering lab tests taken before anesthesia --- ")
+    final_df = merged_df[merged_df['Collection Datetime'] <= merged_df['AN_START_DATETIME']].reset_index(drop = True)
+
+    #group by LOG_ID, MRN and Lab Name
+    # find indices corresponding to the latest Collection Datetime for each group
+    latest_indices = final_df.groupby(['LOG_ID', 'MRN', 'Lab Name'])['Collection Datetime'].idxmax()
+    final_df_filtered = final_df.loc[latest_indices].reset_index(drop = True)
+
+    print(f"Filtering complete. Final Row Count: {len(final_df_filtered)}")
+    # should be 173721
+
+    # Export to csv
+    print("--- Export data ---")
+    output_path = OUTPUT_DATA_PATH + output_file
+    final_df_filtered.to_csv(output_path, index = False)
+    print(f"File saved at {output_path}")
 
 
 if __name__ == "__main__":
