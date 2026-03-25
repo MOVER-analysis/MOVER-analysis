16d0b0e24a2a77bac73716309c4d24468aa8b327
Benjamin Rochwerg <benny.rochwerg@mail.utoronto.ca>
2026-03-25 12:48:19 -0400

    Update EDA Python scripts to reduce redundancy
    Removed functions that were in common with those found in other files (these functions were moved into a new setup.py file)

--- report_eda/log_transform.py
+++ report_eda/log_transform.py
@@ -1,31 +1,7 @@
-# Libraries
+# Library
 import numpy as np
-import pandas as pd
 
-# Functions
-def load_yaml(file_path = "params.yaml"):
-    """
-    Loads the .yaml configuration file.
-    """
-
-    try:
-        with open(file_path, "r") as f:
-            return yaml.safe_load(f)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"File not found: {file_path}")
-
-def load_data(file_path):
-    """
-    Loads a .csv dataset.
-    """
-
-    try:
-        with open(file_path, "r") as f:
-            data = pd.read_csv(f)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"File not found: {file_path}")
-    return data
-
+# Function
 def log_transform(df_file_path_list, var_list):
     """
     Log-transforms skewed variable(s) and saves updated datasets in .csv format.
@@ -63,23 +39,3 @@ def log_transform(df_file_path_list, var_list):
             print(f"Updated dataset saved to: {file_path}")
         except FileNotFoundError:
             raise FileNotFoundError(f"File path not found: {file_path}")
-
-def main():
-
-    # Loading the configuration file and the datasets
-    config = load_yaml()
-    training_path = config["data"]["training_set_path"]
-    test_path = config["data"]["test_set_path"]
-
-    # Setting up df_file_path_list and var_list for log-transformation
-    df_file_path_list = [training_path, test_path]
-    var_list = ["glucose"]
-    
-    # Log-transforming the desired variable(s) and saving the modified datasets
-    log_transform(df_file_path_list, var_list)
-
-    # Printing a message
-    print("Log-transformation process complete.")
-
-if __name__ == "__main__":
-    main()
