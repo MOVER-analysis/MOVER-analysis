4744fa0e5c0093a5c842c3b9e83b59be5cc3a230
Benjamin Rochwerg <benny.rochwerg@mail.utoronto.ca>
2026-03-31 21:39:05 -0400

    Update setup.py to include types
    Specified the types of function inputs and outputs

--- report_main/setup.py
+++ report_main/setup.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Functions
-def load_yaml(file_path = "params.yaml"):
+def load_yaml(file_path: str = "params.yaml") -> dict:
     """
     Loads a .yaml configuration file.
     """
@@ -15,7 +15,7 @@ def load_yaml(file_path = "params.yaml"):
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found: {file_path}")
 
-def load_data(file_path):
+def load_data(file_path: str) -> pd.DataFrame:
     """
     Loads a .csv dataset.
     """
@@ -27,7 +27,7 @@ def load_data(file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
     return data
 
-def create_output_folder(output_folder):
+def create_output_folder(output_folder: str):
     """
     Creates an output folder.
     """
